d3f41c414dc80ab3917b9e49831378219bf437b1
ramyeid <ramy.j.eid@gmail.com>
2021-08-10 09:12:12 +0300

    [JavaUI] Implement Forecast Accuracy UI

--- swing-ui/src/main/java/org/mlsk/ui/components/timeseries/TimeSeriesActionListener.java
+++ swing-ui/src/main/java/org/mlsk/ui/components/timeseries/TimeSeriesActionListener.java
@@ -7,6 +7,7 @@ import org.mlsk.ui.components.utils.TriFunction;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Locale;
 import java.util.concurrent.Callable;
 
 import static org.mlsk.ui.components.utils.ErrorPopup.tryPopup;
@@ -15,15 +16,16 @@ public class TimeSeriesActionListener implements ActionListener {
 
   public static final String PREDICT_COMMAND = "Predict";
   public static final String FORECAST_COMMAND = "Forecast";
-  public static final String FORECAST_VS_ACTUAL = "Forecast vs Actual";
+  public static final String FORECAST_VS_ACTUAL_COMMAND = "Forecast vs Actual";
+  public static final String FORECAST_ACCURACY_COMMAND = "Forecast Accuracy";
 
   private final TimeSeriesInputPanel timeSeriesInputPanel;
-  private final TriFunction<TimeSeries, TimeSeries, String> onResults;
+  private final TriFunction<TimeSeries, Object, String> onResults;
   private final TimeSeriesAnalysisServiceClient timeSeriesAnalysisServiceClient;
 
   public TimeSeriesActionListener(TimeSeriesInputPanel timeSeriesInputPanel,
                                   TimeSeriesAnalysisServiceClient timeSeriesAnalysisServiceClient,
-                                  TriFunction<TimeSeries, TimeSeries, String> onResults) {
+                                  TriFunction<TimeSeries, Object, String> onResults) {
 
     this.timeSeriesInputPanel = timeSeriesInputPanel;
     this.onResults = onResults;
@@ -34,30 +36,38 @@ public class TimeSeriesActionListener implements ActionListener {
   public void actionPerformed(ActionEvent actionEvent) {
     TimeSeriesAnalysisRequest timeSeriesAnalysisRequest = timeSeriesInputPanel.buildTimeSeriesRequest();
     String actionCommand = actionEvent.getActionCommand();
-    Callable<TimeSeries> callable = getCallback(actionCommand, timeSeriesAnalysisRequest);
+    Callable<Object> callable = getCallback(actionCommand, timeSeriesAnalysisRequest);
 
-    TimeSeries computedTimeSeries = tryPopup(callable, String.format("Error while launching %s", actionCommand));
+    Object result = tryPopup(callable, String.format("Error while launching %s", actionCommand));
 
-    onResults.apply(timeSeriesAnalysisRequest.getTimeSeries(), computedTimeSeries, getTimeSeriesTitle(actionCommand));
+    onResults.apply(timeSeriesAnalysisRequest.getTimeSeries(), result, getTimeSeriesTitle(actionCommand));
   }
 
   private String getTimeSeriesTitle(String actionCommand) {
-    if (actionCommand.equals(PREDICT_COMMAND)) {
-      return actionCommand.toLowerCase();
-    } else {
-      return FORECAST_COMMAND.toLowerCase();
+    switch (actionCommand) {
+      case PREDICT_COMMAND:
+      case FORECAST_ACCURACY_COMMAND:
+        return actionCommand.toLowerCase(Locale.ROOT);
+      case FORECAST_COMMAND:
+      case FORECAST_VS_ACTUAL_COMMAND:
+        return FORECAST_COMMAND.toLowerCase(Locale.ROOT);
+      default:
+        return null;
     }
   }
 
-  private Callable<TimeSeries> getCallback(String actionCommand, TimeSeriesAnalysisRequest timeSeriesAnalysisRequest) {
-    Callable<TimeSeries> callable;
-    if (actionCommand.equals(PREDICT_COMMAND)) {
-      callable = () -> timeSeriesAnalysisServiceClient.predict(timeSeriesAnalysisRequest);
-    } else if (actionCommand.equals(FORECAST_COMMAND)) {
-      callable = () -> timeSeriesAnalysisServiceClient.forecast(timeSeriesAnalysisRequest);
-    } else {
-      callable = () -> timeSeriesAnalysisServiceClient.forecastVsActual(timeSeriesAnalysisRequest);
+  private Callable<Object> getCallback(String actionCommand, TimeSeriesAnalysisRequest timeSeriesAnalysisRequest) {
+    switch (actionCommand) {
+      case PREDICT_COMMAND:
+        return () -> timeSeriesAnalysisServiceClient.predict(timeSeriesAnalysisRequest);
+      case FORECAST_COMMAND:
+        return () -> timeSeriesAnalysisServiceClient.forecast(timeSeriesAnalysisRequest);
+      case FORECAST_VS_ACTUAL_COMMAND:
+        return () -> timeSeriesAnalysisServiceClient.forecastVsActual(timeSeriesAnalysisRequest);
+      case FORECAST_ACCURACY_COMMAND:
+        return () -> timeSeriesAnalysisServiceClient.computeForecastAccuracy(timeSeriesAnalysisRequest);
+      default:
+        return () -> null;
     }
-    return callable;
   }
 }
