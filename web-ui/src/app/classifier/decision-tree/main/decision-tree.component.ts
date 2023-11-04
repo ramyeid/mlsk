11f0e0a95d4dc3e705c02ac397bd7d526810a1aa
ramyeid <ramy.j.eid@gmail.com>
2023-11-04 19:41:34 +0200

    [WebUI] Implement Simple DecisionTreeOutputComponent to print results

--- web-ui/src/app/classifier/decision-tree/main/decision-tree.component.ts
+++ web-ui/src/app/classifier/decision-tree/main/decision-tree.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+
+import { InputEmitType } from 'src/app/shared/model/input-emit-type';
+import { ClassifierDataResponse } from '../../model/classifier-data-response';
+import { DecisionTreeOutputComponent } from '../output/decision-tree-output.component';
 
 @Component({
   selector: 'mlsk-decision-tree',
@@ -6,4 +10,13 @@ import { Component } from '@angular/core';
 })
 export class DecisionTreeComponent {
 
+  @ViewChild(DecisionTreeOutputComponent) outputComponent: DecisionTreeOutputComponent;
+
+  onResult(resultAndType: [ClassifierDataResponse | number, InputEmitType]): void {
+    this.outputComponent.onResult(resultAndType);
+  }
+
+  onNewRequest(): void {
+    this.outputComponent.onNewRequest();
+  }
 }
